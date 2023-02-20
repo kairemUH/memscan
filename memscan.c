cc393dbbee4f8fac1f92ce49a8e6914b809548c1
kairemUH <kairem@hawaii.edu>
2023-02-20 01:32:50 -1000

    Added Header

---
+++ memscan.c
@@ -0,0 +1,12 @@
+///////////////////////////////////////////////////////////////////////////////
+///          University of Hawaii, College of Engineering
+///          Lab 4 - memscan - SRE - Spring 2023
+///
+///
+/// memscan - find all legal memory adresses and count number of 'A's in memory
+///
+/// @see     https://linuxcommand.org/lc3_man_pages/wc1.html
+///
+/// @file    wc.c
+/// @author  Kai Matsusaka <kairem@hawaii.edu>
+///////////////////////////////////////////////////////////////////////////////
